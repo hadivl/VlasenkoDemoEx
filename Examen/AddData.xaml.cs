5e4c49868c671fe149cca9f452ff69d5df60d2bb
awaayy <phantomy.design@gmail.com>
2025-04-24 11:38:42 +0300

    commit

--- Examen/AddData.xaml.cs
+++ Examen/AddData.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Examen.Model;
 
 namespace Examen
 {
@@ -34,6 +38,51 @@ namespace Examen
             this.Hide();
             mainWindow.ShowDialog();
             this.Show();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Client newClient = new Client
+            {
+
+                Familiya = FamilyaTextBox.Text,
+                Imya = ImyaTextBox.Text,
+                Otchestvo = OtcgestvoTextBox.Text,
+
+            };
+
+            string connectionString = "Data Source=hqvla3302s01\\KITP;Initial Catalog=Vlasenko_DemoEx;Integrated Security=True;Trust Server Certificate=True";
+            string query = @"
+    INSERT INTO Client (Familiya, Imya, Otchestvo) 
+    VALUES (@Familiya, @Imya, @Otchestvo)";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Familiya", newClient.Familiya);
+                command.Parameters.AddWithValue("@Imya", newClient.Imya);
+                command.Parameters.AddWithValue("@Otchestvo", newClient.Otchestvo);
+
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Новый клиент добавлен успешно.");
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при добавлении клиента: " + ex.Message);
+                }
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
